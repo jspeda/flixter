79674d27ce52f996aca2d48b5e930664182ef79b
Joe Spedale <jspedale@gmail.com>
2016-07-02 03:46:05 +0000

    Added permissions for lessons and fixed navbar

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
+  
+
+
   private
+   
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) != current_user
+      redirect_to courses_path, alert: 'Unauthorized'
+    end
+  end
+
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
